1eb29bcc0fe1f07b949895922a001f0a587d46be
Manuel Rojas <leuamrojas@hotmail.com>
2021-12-16 22:08:53 +0100

    Refactored code

--- KelloxPartnerWCF/Contracts/IKelloxPartnerService.cs
+++ KelloxPartnerWCF/Contracts/IKelloxPartnerService.cs
@@ -8,20 +8,20 @@ namespace KelloxPartnerWCF.Contracts
     [ServiceContract]
     public interface IKelloxPartnerService
     {
-        [OperationContract]
-        [WebInvoke(
-            Method = "POST",
-            UriTemplate = "/ReceiveOrder",
-            ResponseFormat = WebMessageFormat.Xml)]
-        XmlElement ReceiveOrder(Stream dataStream);
-
         //[OperationContract]
         //[WebInvoke(
         //    Method = "POST",
         //    UriTemplate = "/ReceiveOrder",
-        //    BodyStyle = WebMessageBodyStyle.Bare,
-        //    RequestFormat = WebMessageFormat.Xml,
         //    ResponseFormat = WebMessageFormat.Xml)]
-        //XmlElement ReceiveOrder(Stream orderInputXml);
+        //XmlElement ReceiveOrder(Stream dataStream);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "POST",
+            UriTemplate = "/ReceiveOrder",
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml)]
+        XmlElement ReceiveOrder(Stream data);
     }
 }
